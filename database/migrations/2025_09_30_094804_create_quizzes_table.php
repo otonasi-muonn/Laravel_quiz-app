fb1939b447d122a4f70f0d67bee61959a52f03a0
otonasi-muonn <nayudoutianzhong@gmail.com>
2025-09-30 19:41:16 +0900

    DB基盤

--- database/migrations/2025_09_30_094804_create_quizzes_table.php
+++ database/migrations/2025_09_30_094804_create_quizzes_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('quizzes', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->unique()->comment('クイズのタイトル');
+            $table->text('question')->comment('問題文');
+            $table->string('option_a')->comment('選択肢A');
+            $table->string('option_b')->comment('選択肢B');
+            $table->string('option_c')->comment('選択肢C');
+            $table->string('option_d')->comment('選択肢D');
+            $table->unsignedTinyInteger('correct_option')->comment('正解の選択肢番号(1-4)');
+            $table->text('explanation')->nullable()->comment('解説文');
+            $table->boolean('is_published')->default(true)->comment('公開状態')->index();
             $table->timestamps();
         });
     }
